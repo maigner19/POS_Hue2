575b76ec7fa3e0e01fe8a5194a110f6a4158cf19
AignerM190024 <AignerM190024@sus.htl-grieskirchen.at>
2021-10-19 08:26:07 +0200

    commit

---
+++ hu2/src/hu2/NumberTester.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu2;
+
+/**
+ *
+ * @author Michael
+ */
+public class NumberTester {
+
+    public NumberTester(String fileName) {
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+
+    }
+}
